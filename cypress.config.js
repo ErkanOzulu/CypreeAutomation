e94cd1f80d61cf51e76ead5a62be2434e41f61df
Erkan <ErkanOzulu@users.noreply.github.com>
2023-05-28 16:24:45 +0300

    added new locator strategies

--- cypress.config.js
+++ cypress.config.js
@@ -3,9 +3,10 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl:'https://practice.cydeo.com/',
-git a
+
     video:false, // it prevent to generate video or stop to default generate video
-    retries: 2,// if test failes it runs 2 more times
+    retries: 1,// if test failes it runs 2 more times
+    defaultCommandTimeout: 5000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
