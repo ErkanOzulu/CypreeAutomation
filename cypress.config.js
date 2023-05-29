7ae0922b323fc5017e72446a6f6844ba109f7ee3
Erkan <ErkanOzulu@users.noreply.github.com>
2023-05-29 17:17:44 +0300

    add:viewportHeight_Width

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,8 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl:'https://practice.cydeo.com/',
-
+    viewportHeight:800,
+    viewportWidth:1200,
     video:false, // it prevent to generate video or stop to default generate video
     retries: 1,// if test failes it runs 2 more times
     defaultCommandTimeout: 5000,
